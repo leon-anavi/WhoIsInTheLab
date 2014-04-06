5467a009b1f9a696617b94934b1bfb6505d756c8
Rangel Ivanov <rangelivanov88@gmail.com>
2014-04-06 22:02:56 +0300

    Implemened retrieval of mac address from arp table

--- lab-registration/registration.go
+++ lab-registration/registration.go
@@ -1,5 +1,42 @@
 package main
 
-func Hello() string {
-	return "Hello World!";
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"os/exec"
+	"io/ioutil"
+	"log"
+)
+
+func check_error(err error) {
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+}
+
+func exec_arp_search(arp_file string, ipAddr string) string {
+	arp_list := exec.Command("cat", arp_file)
+	out, err := arp_list.Output()
+	check_error(err)
+
+	ioutil.WriteFile("/tmp/arp_list", out, 0644)
+
+	out_grep, err := exec.Command("grep", "-w" , ipAddr, "/tmp/arp_list").Output()
+	check_error(err)
+
+	return string(out_grep[:])
+
+}
+
+func GetMacAddress(arp_file string, ipaddr string) string {
+	arp_info := exec_arp_search(arp_file, ipaddr)
+	fields := strings.Fields(arp_info)
+	return fields[3]
+}
+
+func Hello(res http.ResponseWriter, req *http.Request) string {
+	ipaddr := strings.Split(req.RemoteAddr, ":")[0]
+	return GetMacAddress("/proc/net/arp", ipaddr)
 }
--- lab-registration/registration_test.go
+++ lab-registration/registration_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 )
 
-func TestThatPrintsHelloWorld(t *testing.T) {
-	expected := "Hello World!"
-	actual := Hello()
-	if actual != expected {
-		t.Errorf("expected %v, but was %v", expected, actual)
+func TestGetMacAddressFromArpTable(t *testing.T) {
+	ipaddr := "192.168.50.1"
+	expected_mac := "00:16:0a:13:96:7e"
+	actual := GetMacAddress("./test-data/arp-data", ipaddr)
+	if actual != expected_mac {
+		t.Errorf("expected %v, but was %v", expected_mac, actual)
 	}
 }
