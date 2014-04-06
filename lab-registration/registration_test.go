bc1d825fa308215aab67c6ec244fb0b33a9c76c5
Rangel Ivanov <rangelivanov88@gmail.com>
2014-04-06 23:25:05 +0300

    Forgot to replace theese

--- lab-registration/registration_test.go
+++ lab-registration/registration_test.go
@@ -8,6 +8,6 @@ func TestGetMacAddressFromArpTable(t *testing.T) {
 	want := "00:16:0a:13:96:7e"
 	got := GetMacAddress("./test-data/arp-data", "192.168.50.1")
 	if want != got {
-		t.Errorf("expected %v, but was %v", expected_mac, actual)
+		t.Errorf("expected %v, but was %v", want, got)
 	}
 }
