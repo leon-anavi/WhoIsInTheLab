756d31033bb3891f2016ee0fda0299f7770d6bff
Rangel Ivanov <rangelivanov88@gmail.com>
2014-04-07 00:38:52 +0300

    Added some error reporting

--- lab-registration/registration.go
+++ lab-registration/registration.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"os/exec"
 	"log"
+	"fmt"
 )
 
 func checkError(err error) {
@@ -13,21 +14,32 @@ func checkError(err error) {
 	}
 }
 
-func getArpEntry(arpFile string, ip string) string {
+func getArpEntry(arpFile string, ip string) (string, error) {
 	grepOut, err := exec.Command("grep", "-w" , ip, arpFile).Output()
-	checkError(err)
-
-	return string(grepOut[:])
+	if err != nil {
+		return "", err
+	}
+	return string(grepOut[:]), nil
 }
 
-func GetMacAddress(arpFile string, ip string) string {
-	entry := getArpEntry(arpFile, ip)
+func GetMacAddress(arpFile string, ip string) (string, error) {
+	if len(ip) == 0 {
+		return "", fmt.Errorf("ip address cannot be empty")
+	}
+	entry, err := getArpEntry(arpFile, ip)
+	if err != nil {
+		return "", fmt.Errorf("Cannot find MAC address for ip: %v", ip)
+	}
 	fields := strings.Fields(entry)
 	// This is the mac address field
-	return fields[3]
+	return fields[3], nil
 }
 
 func Hello(res http.ResponseWriter, req *http.Request) string {
 	ip := strings.Split(req.RemoteAddr, ":")[0]
-	return GetMacAddress("/proc/net/arp", ip)
+	mac, err := GetMacAddress("/proc/net/arp", ip)
+	if err != nil {
+		return err.Error()
+	}
+	return mac
 }
--- lab-registration/registration_test.go
+++ lab-registration/registration_test.go
@@ -6,8 +6,32 @@ import (
 
 func TestGetMacAddressFromArpTable(t *testing.T) {
 	want := "00:16:0a:13:96:7e"
-	got := GetMacAddress("./test-data/arp-data", "192.168.50.1")
+	got, _ := GetMacAddress("./test-data/arp-data", "192.168.50.1")
 	if want != got {
 		t.Errorf("expected %v, but was %v", want, got)
+	}
+}
+
+func TestFailureWithInvalidArpFile(t *testing.T) {
+	_, err := GetMacAddress("/some/nonexisting/file", "192.168.50.1")
+
+	if err == nil {
+		t.Errorf("should return error since file dosent exist")
+	}
+}
+
+func TestFailureWithNonExistingIp(t *testing.T) {
+	_, err := GetMacAddress("./test-data/arp-data", "192.168.50.234")
+
+	if err == nil {
+		t.Errorf("should return error since this ip dosen't exist")
+	}
+}
+
+func TestFailureWithEmptyIp(t *testing.T) {
+	_, err := GetMacAddress("./test-data/arp-data", "")
+
+	if err == nil {
+		t.Errorf("should return error since ip address is empty")
 	}
 }
